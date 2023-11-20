e65129d32e003bb978009a1adbdfb985d44e84bb
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-11-20 01:16:28 -0500

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import loaders  from 'react-loader-spinner';
-import css from "./Loader.module.css";
+import React from 'react';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import loaders from 'react-loader-spinner';
+import css from './Loader.module.css';
 
 const LoaderSpinner = () => {
   return (
     <div className={css.container}>
-      <loaders.BallTriangle
-        color="#3f51b5"  
+      <loaders.Oval
         height={80}
         width={80}
-        timeout={3000} 
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
       />
     </div>
   );
